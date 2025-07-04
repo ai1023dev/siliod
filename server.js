6468560e24695d43b73ef0c91c389ab528fc051d
ai1023dev <ai1023dev@gmail.com>
2025-07-04 22:27:08 +0900

    설트봇 폐기, Buypass경로로 수정

--- server.js
+++ server.js
@@ -517,7 +517,7 @@ async function startServer() {
                     "sudo make -C /home/ubuntu/.ttyd/build install",
                     "curl https://get.acme.sh | sh",
                     "~/.acme.sh/acme.sh --set-default-ca --server https://api.buypass.com/acme/directory",
-                    `~/.acme.sh/acme.sh --issue --standalone -d ${domain} --accountemail siliod.official@gmail.com`,
+                    `~/.acme.sh/acme.sh --issue --debug --standalone -d ${domain} --accountemail siliod.official@gmail.com`,
                     `~/.acme.sh/acme.sh --install-cert -d ${domain} --key-file /etc/ssl/private/${domain}.key --fullchain-file /etc/ssl/certs/${domain}.crt`,
                 ];
 
